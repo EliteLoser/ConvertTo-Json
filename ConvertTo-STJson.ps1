04be8a8e0f7b97d881c374284f092fd5fa28986f
Joakim Svendsen <joakimbs@gmail.com>
2018-07-15 01:20:51 +0200

    v1.0. Improve readability my own way

--- ConvertTo-STJson.ps1
+++ ConvertTo-STJson.ps1
@@ -1,4 +1,5 @@
-# Author: Joakim Borger Svendsen, 2017. http://www.json.org
+# Author: Joakim Borger Svendsen, 2017. 
+# JSON info: http://www.json.org
 # Svendsen Tech. Public domain licensed code.
 # v0.3, 2017-04-12 (second release of the day, I actually read some JSON syntax this time)
 #       Fixed so you don't double-whack the allowed escapes from the diagram, not quoting null, false and true as values.
@@ -39,12 +40,13 @@
 #           '2018-06-25T01:25:00').
 # v0.9.5.1: Handle "infinity" as well for System.Double.
 # v0.9.5.2: Fix bug with DateTime ISO formatting inside hash tables and PS objects.
+# v1.0: Improve readability, by "popular" demand...
 ######################################################################################################
 
 # Take care of special characters in JSON (see json.org), such as newlines, backslashes
 # carriage returns and tabs.
 # '\\(?!["/bfnrt]|u[0-9a-f]{4})'
-function FormatString {
+function EscapeJson {
     param(
         [String] $String)
     # removed: #-replace '/', '\/' `
@@ -74,7 +76,7 @@ function GetNumberOrString {
     }
     else {
         Write-Verbose -Message "Got a string (or 'NaN') as end value."
-        """$(FormatString -String $InputObject)"""
+        """$(EscapeJson -String $InputObject)"""
     }
 }
 
@@ -82,154 +84,209 @@ function ConvertToJsonInternal {
     param(
         $InputObject, # no type for a reason
         [Int32] $WhiteSpacePad = 0)
+    
     [String] $Json = ""
+    
     $Keys = @()
+    
     Write-Verbose -Message "WhiteSpacePad: $WhiteSpacePad."
+    
     if ($null -eq $InputObject) {
         Write-Verbose -Message "Got 'null' in `$InputObject in inner function"
         $null
     }
+    
     elseif ($InputObject -is [Bool] -and $InputObject -eq $true) {
         Write-Verbose -Message "Got 'true' in `$InputObject in inner function"
         $true
     }
+    
     elseif ($InputObject -is [Bool] -and $InputObject -eq $false) {
         Write-Verbose -Message "Got 'false' in `$InputObject in inner function"
         $false
     }
+    
     elseif ($InputObject -is [DateTime] -and $Script:DateTimeAsISO8601) {
         Write-Verbose -Message "Got a DateTime and will format it as ISO 8601."
         """$($InputObject.ToString('yyyy\-MM\-ddTHH\:mm\:ss'))"""
     }
+    
     elseif ($InputObject -is [HashTable]) {
-        $Keys = @($InputObject.Keys)
         Write-Verbose -Message "Input object is a hash table (keys: $($Keys -join ', '))."
+        $Keys = @($InputObject.Keys)
     }
+    
     elseif ($InputObject.GetType().FullName -eq "System.Management.Automation.PSCustomObject") {
         $Keys = @(Get-Member -InputObject $InputObject -MemberType NoteProperty |
             Select-Object -ExpandProperty Name)
+
         Write-Verbose -Message "Input object is a custom PowerShell object (properties: $($Keys -join ', '))."
     }
+    
     elseif ($InputObject.GetType().Name -match '\[\]|Array') {
-        Write-Verbose -Message "Input object appears to be of a collection/array type."
-        Write-Verbose -Message "Building JSON for array input object."
+        
+        Write-Verbose -Message "Input object appears to be of a collection/array type. Building JSON for array input object."
+        
         $Json += "[`n" + (($InputObject | ForEach-Object {
+            
             if ($null -eq $_) {
                 Write-Verbose -Message "Got null inside array."
+
                 " " * ((4 * ($WhiteSpacePad / 4)) + 4) + "null"
             }
+            
             elseif ($_ -is [Bool] -and $_ -eq $true) {
                 Write-Verbose -Message "Got 'true' inside array."
+
                 " " * ((4 * ($WhiteSpacePad / 4)) + 4) + "true"
             }
+            
             elseif ($_ -is [Bool] -and $_ -eq $false) {
                 Write-Verbose -Message "Got 'false' inside array."
+
                 " " * ((4 * ($WhiteSpacePad / 4)) + 4) + "false"
             }
+            
             elseif ($_ -is [DateTime] -and $Script:DateTimeAsISO8601) {
                 Write-Verbose -Message "Got a DateTime and will format it as ISO 8601."
+
                 " " * ((4 * ($WhiteSpacePad / 4)) + 4) + """$($_.ToString('yyyy\-MM\-ddTHH\:mm\:ss'))"""
             }
+            
             elseif ($_ -is [HashTable] -or $_.GetType().FullName -eq "System.Management.Automation.PSCustomObject" -or $_.GetType().Name -match '\[\]|Array') {
                 Write-Verbose -Message "Found array, hash table or custom PowerShell object inside array."
+
                 " " * ((4 * ($WhiteSpacePad / 4)) + 4) + (ConvertToJsonInternal -InputObject $_ -WhiteSpacePad ($WhiteSpacePad + 4)) -replace '\s*,\s*$'
             }
+            
             else {
                 Write-Verbose -Message "Got a number or string inside array."
+
                 $TempJsonString = GetNumberOrString -InputObject $_
                 " " * ((4 * ($WhiteSpacePad / 4)) + 4) + $TempJsonString
             }
+
         }) -join ",`n") + "`n$(" " * (4 * ($WhiteSpacePad / 4)))],`n"
+
     }
     else {
         Write-Verbose -Message "Input object is a single element (treated as string/number)."
+
         GetNumberOrString -InputObject $InputObject
     }
     if ($Keys.Count) {
+
         Write-Verbose -Message "Building JSON for hash table or custom PowerShell object."
+
         $Json += "{`n"
+
         foreach ($Key in $Keys) {
+
             # -is [PSCustomObject]) { # this was buggy with calculated properties, the value was thought to be PSCustomObject
+
             if ($null -eq $InputObject.$Key) {
                 Write-Verbose -Message "Got null as `$InputObject.`$Key in inner hash or PS object."
                 $Json += " " * ((4 * ($WhiteSpacePad / 4)) + 4) + """$Key"": null,`n"
             }
+
             elseif ($InputObject.$Key -is [Bool] -and $InputObject.$Key -eq $true) {
                 Write-Verbose -Message "Got 'true' in `$InputObject.`$Key in inner hash or PS object."
                 $Json += " " * ((4 * ($WhiteSpacePad / 4)) + 4) + """$Key"": true,`n"            }
+
             elseif ($InputObject.$Key -is [Bool] -and $InputObject.$Key -eq $false) {
                 Write-Verbose -Message "Got 'false' in `$InputObject.`$Key in inner hash or PS object."
                 $Json += " " * ((4 * ($WhiteSpacePad / 4)) + 4) + """$Key"": false,`n"
             }
+
             elseif ($InputObject.$Key -is [DateTime] -and $Script:DateTimeAsISO8601) {
                 Write-Verbose -Message "Got a DateTime and will format it as ISO 8601."
                 $Json += " " * ((4 * ($WhiteSpacePad / 4)) + 4) + """$Key"": ""$($InputObject.$Key.ToString('yyyy\-MM\-ddTHH\:mm\:ss'))"",`n"
                 
             }
+
             elseif ($InputObject.$Key -is [HashTable] -or $InputObject.$Key.GetType().FullName -eq "System.Management.Automation.PSCustomObject") {
                 Write-Verbose -Message "Input object's value for key '$Key' is a hash table or custom PowerShell object."
                 $Json += " " * ($WhiteSpacePad + 4) + """$Key"":`n$(" " * ($WhiteSpacePad + 4))"
                 $Json += ConvertToJsonInternal -InputObject $InputObject.$Key -WhiteSpacePad ($WhiteSpacePad + 4)
             }
+
             elseif ($InputObject.$Key.GetType().Name -match '\[\]|Array') {
+
                 Write-Verbose -Message "Input object's value for key '$Key' has a type that appears to be a collection/array."
                 Write-Verbose -Message "Building JSON for ${Key}'s array value."
+
                 $Json += " " * ($WhiteSpacePad + 4) + """$Key"":`n$(" " * ((4 * ($WhiteSpacePad / 4)) + 4))[`n" + (($InputObject.$Key | ForEach-Object {
-                    #Write-Verbose "Type inside array inside array/hash/PSObject: $($_.GetType().FullName)"
+
                     if ($null -eq $_) {
                         Write-Verbose -Message "Got null inside array inside inside array."
                         " " * ((4 * ($WhiteSpacePad / 4)) + 8) + "null"
                     }
+
                     elseif ($_ -is [Bool] -and $_ -eq $true) {
                         Write-Verbose -Message "Got 'true' inside array inside inside array."
                         " " * ((4 * ($WhiteSpacePad / 4)) + 8) + "true"
                     }
+
                     elseif ($_ -is [Bool] -and $_ -eq $false) {
                         Write-Verbose -Message "Got 'false' inside array inside inside array."
                         " " * ((4 * ($WhiteSpacePad / 4)) + 8) + "false"
                     }
+
                     elseif ($_ -is [DateTime] -and $Script:DateTimeAsISO8601) {
                         Write-Verbose -Message "Got a DateTime and will format it as ISO 8601."
                         " " * ((4 * ($WhiteSpacePad / 4)) + 8) + """$($_.ToString('yyyy\-MM\-ddTHH\:mm\:ss'))"""
                     }
+
                     elseif ($_ -is [HashTable] -or $_.GetType().FullName -eq "System.Management.Automation.PSCustomObject" `
                         -or $_.GetType().Name -match '\[\]|Array') {
                         Write-Verbose -Message "Found array, hash table or custom PowerShell object inside inside array."
                         " " * ((4 * ($WhiteSpacePad / 4)) + 8) + (ConvertToJsonInternal -InputObject $_ -WhiteSpacePad ($WhiteSpacePad + 8)) -replace '\s*,\s*$'
                     }
+
                     else {
                         Write-Verbose -Message "Got a string or number inside inside array."
                         $TempJsonString = GetNumberOrString -InputObject $_
                         " " * ((4 * ($WhiteSpacePad / 4)) + 8) + $TempJsonString
                     }
+
                 }) -join ",`n") + "`n$(" " * (4 * ($WhiteSpacePad / 4) + 4 ))],`n"
+
             }
             else {
+
                 Write-Verbose -Message "Got a string inside inside hashtable or PSObject."
                 # '\\(?!["/bfnrt]|u[0-9a-f]{4})'
+
                 $TempJsonString = GetNumberOrString -InputObject $InputObject.$Key
                 $Json += " " * ((4 * ($WhiteSpacePad / 4)) + 4) + """$Key"": $TempJsonString,`n"
+
             }
+
         }
+
         $Json = $Json -replace '\s*,$' # remove trailing comma that'll break syntax
         $Json += "`n" + " " * $WhiteSpacePad + "},`n"
+
     }
+
     $Json
+
 }
 
 function ConvertTo-STJson {
     [CmdletBinding()]
     #[OutputType([Void], [Bool], [String])]
-    param(
+    Param(
         [AllowNull()]
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$True,
+                   ValueFromPipeline=$True,
+                   ValueFromPipelineByPropertyName=$True)]
         $InputObject,
         [Switch] $Compress,
-        [Switch] $CoerceNumberStrings = $false,
-        [Switch] $DateTimeAsISO8601 = $false)
-    begin{
+        [Switch] $CoerceNumberStrings = $False,
+        [Switch] $DateTimeAsISO8601 = $False)
+    Begin{
+
         $JsonOutput = ""
         $Collection = @()
         # Not optimal, but the easiest now.
@@ -237,34 +294,45 @@ function ConvertTo-STJson {
         [Bool] $Script:DateTimeAsISO8601 = $DateTimeAsISO8601
         [String] $Script:NumberRegex = '^-?\d+(?:(?:\.\d+)?(?:e[+\-]?\d+)?)?$'
         #$Script:NumberAndValueRegex = '^-?\d+(?:(?:\.\d+)?(?:e[+\-]?\d+)?)?$|^(?:true|false|null)$'
+
     }
-    process {
+
+    Process {
+
         # Hacking on pipeline support ...
         if ($_) {
             Write-Verbose -Message "Adding object to `$Collection. Type of object: $($_.GetType().FullName)."
             $Collection += $_
         }
+
     }
-    end {
+
+    End {
+        
         if ($Collection.Count) {
             Write-Verbose -Message "Collection count: $($Collection.Count), type of first object: $($Collection[0].GetType().FullName)."
             $JsonOutput = ConvertToJsonInternal -InputObject ($Collection | ForEach-Object { $_ })
         }
+        
         else {
             $JsonOutput = ConvertToJsonInternal -InputObject $InputObject
         }
+        
         if ($null -eq $JsonOutput) {
             Write-Verbose -Message "Returning `$null."
             return $null # becomes an empty string :/
         }
+        
         elseif ($JsonOutput -is [Bool] -and $JsonOutput -eq $true) {
             Write-Verbose -Message "Returning `$true."
             [Bool] $true # doesn't preserve bool type :/ but works for comparisons against $true
         }
+        
         elseif ($JsonOutput-is [Bool] -and $JsonOutput -eq $false) {
             Write-Verbose -Message "Returning `$false."
             [Bool] $false # doesn't preserve bool type :/ but works for comparisons against $false
         }
+        
         elseif ($Compress) {
             Write-Verbose -Message "Compress specified."
             (
@@ -276,9 +344,12 @@ function ConvertTo-STJson {
                     ')))\s*(?<Comma>,)?\s*$'), "`${1}:`${2}`${Comma}`n" `
               -replace '(?m)^\s*|\s*\z|[\r\n]+'
         }
+        
         else {
             ($JsonOutput -split "\n" | Where-Object { $_ -match '\S' }) -join "`n" `
                 -replace '^\s*|\s*,\s*$' -replace '\ *\]\ *$', ']'
         }
+    
     }
+
 }
